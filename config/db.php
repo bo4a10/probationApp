a34dfc0c5f28d4b71bf6a6e23e915d5cb7b23a9f
nail <nail.khamzin@gmail.com>
2016-03-18 10:23:29 +0300

    change User model

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=probation',
     'username' => 'root',
-    'password' => '18724971',
+    'password' => '187249',
     'charset' => 'utf8',
 ];
